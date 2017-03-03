b9990561c1e49caa774c159e0a8e5c1fe63249d2
badrak <andrewbadrak29@gmail.com>
2017-03-03 13:20:50 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -3,16 +3,18 @@ class Card(object):
     def __init__(self, rank, suit, isvisible = False):
         self.rank = rank
         self.suit = suit
+        self.isvisible = isvisible
 
 
 class Table:
     class Deck:
-        def __init__(self):
+        def __init__(self, card):
             self.rank = range(1, 14)
             self.suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+            self.card = card
             deck = []
-            for i in suit:
-                for j in rank:
+            for i in self.suit:
+                for j in self.rank:
                     deck.append(card(j, i))
 
         def shuffled(self):

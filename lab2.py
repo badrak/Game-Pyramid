34a006f2a3cd71eb39b355a3c6acf6687a736314
badrak <andrewbadrak29@gmail.com>
2017-03-07 17:59:58 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,22 +1,34 @@
 class Card(object):
-
-    def __init__(self, rank, suit, isvisible = False):
+    def __init__(self, rank, suit, isvisible=False):
         self.rank = rank
         self.suit = suit
         self.isvisible = isvisible
 
 
-class Table:
-    class Deck:
-        def __init__(self, card):
-            self.rank = range(1, 14)
-            self.suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-            self.card = card
-            deck = []
-            for i in self.suit:
-                for j in self.rank:
-                    deck.append(card(j, i))
+class Deck:
+    def __init__(self):
+        self.rank = range(1, 14)
+        self.suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        self.deck = []
+        for suit in self.suit:
+            for rank in self.rank:
+                self.deck.append(Card(rank, suit))
 
-        def shuffled(self):
-            import random
-            random.shuffle(self)
+    def __next__(self):
+        return self.deck.pop(0)
+
+
+class Pyramid:
+    def __init__(self):
+        self.cards_pyramid = []
+
+    def __str__(self):
+        return self.cards_pyramid
+
+
+class Game:
+    def __init__(self):
+        self.deck = Deck()
+        self.pyramid = Pyramid()
+        for i in range(28):
+            self.pyramid.cards_pyramid.append(self.deck.__next__())

35d2ce3a709b29e4529ccb7ccdc961ea1a9e0dfd
badrak <andrewbadrak29@gmail.com>
2017-03-08 16:38:09 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,8 +1,14 @@
+import random
+
+
 class Card(object):
     def __init__(self, rank, suit, isvisible=False):
         self.rank = rank
         self.suit = suit
         self.isvisible = isvisible
+
+    def __repr__(self):
+        return"%s %s" % (self.suit, self.rank)
 
 
 class Deck:
@@ -13,6 +19,10 @@ class Deck:
         for suit in self.suit:
             for rank in self.rank:
                 self.deck.append(Card(rank, suit))
+        random.shuffle(self.deck)
+
+    def __str__(self):
+        return self.deck
 
     def __next__(self):
         return self.deck.pop(0)
@@ -32,3 +42,12 @@ class Game:
         self.pyramid = Pyramid()
         for i in range(28):
             self.pyramid.cards_pyramid.append(self.deck.__next__())
+
+    def __str__(self):
+        return self.pyramid.cards_pyramid
+
+if __name__ == "__main__":
+    my_deck = Deck()
+    print('Полная колода - ', my_deck.__str__())
+    my_game = Game()
+    print('Колода пирамиды - ', my_game.__str__())
